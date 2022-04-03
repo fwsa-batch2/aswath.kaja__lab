672d193b6d365a6c6f7b2dc5f97714343f1c8ac4
ASWATH48 <kingaswath46@gmail.com>
2022-04-03 13:55:20 +0530

    added Arithmetic assignment operator java file

---
+++ Java - Labworks/arithmetic_assignment_operator.java
@@ -0,0 +1,17 @@
+public class arithmetic_assignment_operator {
+    public static void main(String[] args) {
+ // 2. Arithmetic assignment operators
+ int a = 10;
+ System.out.println("VALUE OF A      :"+  a);
+     a+=5;
+ System.out.println("ADDITION        :"+ (a) );
+     a-=5;
+ System.out.println("SUBTRACTION     :"+ (a) );
+     a*=5;
+ System.out.println("MULTIPLICATION  :"+ (a) );
+     a/=5;
+ System.out.println("DIVISION        :"+ (a) );
+    a%=5;
+ System.out.println("MODULUS         :"+ (a) );
+    }
+}
