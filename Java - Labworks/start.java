dcedfb05fdab152f516a0d13665dbd93d8c69c0b
ASWATH48 <kingaswath46@gmail.com>
2022-04-03 13:09:29 +0530

    added variable java file

---
+++ Java - Labworks/start.java
@@ -0,0 +1,19 @@
+public class start{
+    public static void main(String[] args) {
+//type casting
+        //wide castening
+        int a = 10;
+        double b = a;
+
+        //narrow castening
+        double c = 25.55;
+        int d = (int) c;
+        System.out.println(d);
+
+ //arithemtic operators
+ 
+
+
+
+    }
+}
---
+++ Java - Labworks/variables.java
@@ -0,0 +1,15 @@
+public class variables {
+    //Declaring variables 
+    public static void main(String[] args) {
+    String name = "Aswath";
+    int age = 18;
+    char gender = 'M';
+    float height = 18.8f;
+    boolean is_married  = false;
+    System.out.println("Name :" + name );
+    System.out.println("Age :" + age );
+    System.out.println("Gender :" + gender );
+    System.out.println("Height :" + height);
+    System.out.println("Marrital status:" + is_married );
+    }
+}
