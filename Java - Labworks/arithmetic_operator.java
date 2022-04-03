3430def99766a05b7572b0f6f83c9d8b09cbfc2a
ASWATH48 <kingaswath46@gmail.com>
2022-04-03 13:43:57 +0530

    added Assignment java file

---
+++ Java - Labworks/arithmetic_operator.java
@@ -0,0 +1,12 @@
+public class arithmetic_operator{
+    public static void main(String[] args) {
+ // arithmetic operators
+    int a = 10, b = 5;
+    System.out.println("ADDITION        :"+ (a+b) );
+    System.out.println("SUBTRACTION     :"+ (a-b) );
+    System.out.println("MULTIPLICATION  :"+ (a*b) );
+    System.out.println("DIVISION        :"+ (a/b) );
+    System.out.println("MODULUS         :"+ (a%b) );
+
+    }
+}
