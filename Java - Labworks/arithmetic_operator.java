9c47f39797c49527576fe8711d7a19d24e282610
ASWATH48 <kingaswath46@gmail.com>
2022-04-03 13:46:11 +0530

    added Arithmetic operator java file

--- Java - Labworks/arithmetic_operator.java
+++ Java - Labworks/arithmetic_operator.java
@@ -1,6 +1,6 @@
 public class arithmetic_operator{
     public static void main(String[] args) {
- // arithmetic operators
+ // 1. Arithmetic operators
     int a = 10, b = 5;
     System.out.println("ADDITION        :"+ (a+b) );
     System.out.println("SUBTRACTION     :"+ (a-b) );
