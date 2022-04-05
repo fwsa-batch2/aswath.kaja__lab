92fce36af57b3c0440fc5ef944d4c32f920892b1
ASWATH48 <kingaswath46@gmail.com>
2022-04-05 22:54:06 +0530

    added relational_operator

---
+++ Java - Labworks/relational_operator.java
@@ -0,0 +1,21 @@
+public class relational_operator {
+    public static void main(String[] args) {
+/*
+    Relational Operators     
+   == - equal operator.          
+   != - non-equal operator. 
+   <  - lesser than operator.
+   >  - greater than operator.
+   <= - less than or equal to operator.
+   >=  - is greater than or equal to operator.
+*/
+   int a=5,b =10;
+   System.out.println("Equal                    :"+ (a==b));
+   System.out.println("Not Equal                :"+ (a!=b));
+   System.out.println("Lesser than              :"+ (a<b));
+   System.out.println("Greater than             :"+ (a>b));
+   System.out.println("Lesser than or Equal  to :"+ (a<=b));
+   System.out.println("Greater than or equal to :"+ (a>=b));
+
+    }
+}
