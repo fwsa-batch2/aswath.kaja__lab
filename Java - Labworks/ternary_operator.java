3c480bc6c2627ce2cac422f88cbc8771aa15a65d
ASWATH48 <kingaswath46@gmail.com>
2022-04-07 11:57:27 +0530

    added ternary operator

---
+++ Java - Labworks/ternary_operator.java
@@ -0,0 +1,19 @@
+public class ternary_operator {
+public static void main(String[] args) {
+   /* ternary operator or conditional operator
+   SYNTAX: variable = (condition) ? expression1 : expression2  
+
+  The given condition is true then the variables value will be expression 1 if 
+       it is false then it will be expression 2
+   */
+  int a = 5 , b = 10 , c =a >b?a:b;
+  /*
+    If the given condition is true then the value of c will be value of a , if it is false then the value of v will
+    be value of b
+  */
+  System.out.println(c);
+   
+
+    }
+    
+}
